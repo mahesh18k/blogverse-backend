b61ae256681403bfbb9fbbcb415d0deb6def1c85
Mahesh Kunchala <maheshkunchala18@gmail.com>
2024-07-18 22:05:31 +0530

    Add profile model

---
+++ Models/profileModel.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const profileSchema = new mongoose.Schema({
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        unique: true
+    },
+    total_blogs: {
+        type: Number,
+        default: 0
+    },
+    total_views: {
+        type: Number,
+        default: 0
+    },
+    total_upvotes: {
+        type: Number,
+        default: 0
+    },
+    total_downvotes: {
+        type: Number,
+        default: 0
+    },
+    blogs: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Blog'
+    }]
+});
+
+const Profile = mongoose.model('Profile', profileSchema);
+
+export default Profile;
